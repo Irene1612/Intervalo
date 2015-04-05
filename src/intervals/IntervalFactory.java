639819fa5026b2ce8463fcaff8dfaf996353103b
Irene1612 <vicmatil.igf@gmail.com>
2015-04-05 18:01:10 +0200

    Actualizada IntervalFactory. En función del Opening se crea un mínimo ExactEndpoint o FromEndpoint y un máximo ExactEndpoint o ToEndpoint.

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -8,16 +8,16 @@ public class IntervalFactory {
 		
 		switch (opening) {
 			case BOTH_OPENED:
-				interval = new BothOpenedInterval(minimum, maximum);
+				interval = new BothOpenedInterval(new FromEndpoint(minimum), new ToEndpoint(maximum));
 				break;
 			case RIGHT_OPENED:
-				interval = new RightOpenedInterval(minimum, maximum);
+				interval = new RightOpenedInterval(new ExactEndpoint(minimum), new ToEndpoint(maximum));
 				break;
 			case LEFT_OPENED:
-				interval = new LeftOpenedInterval(minimum, maximum);
+				interval = new LeftOpenedInterval(new FromEndpoint(minimum), new ExactEndpoint(maximum));
 				break;
 			default: // UNOPENED
-				interval = new UnopenedInterval(minimum, maximum);
+				interval = new UnopenedInterval(new ExactEndpoint(minimum), new ExactEndpoint(maximum));
 				break;
 		}
 		return interval;
